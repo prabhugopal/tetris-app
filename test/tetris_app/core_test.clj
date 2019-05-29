ab795db439d157e74bf8c8a8e707438ce972a092
Prabhu Gopal <prabhugopal@Prabhus-MBP.hsd1.md.comcast.net>
2019-05-29 03:57:13 -0400

    Updated the tests, comments.

--- test/tetris_app/core_test.clj
+++ test/tetris_app/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [tetris-app.core :refer :all]))
 
+
+;;test for checking the get-shape-orientation
 (deftest test-get-shape-orientation
   (testing "get-shape-orientation.."
     (is 
@@ -10,6 +12,7 @@
       ['(1 0) '(1 1)
        '(0 0) '(0 1)]))))
 
+;;test for checking the valid row for the given shape
 (deftest test-get-valid-row
   (testing "get-valid-row.."
     (let [state [#{0 1 2 3}]
@@ -21,13 +24,14 @@
         (get-valid-row shape col state size) 
         1)))))
 
-
+;;test for checking the can-shape-fit? method.
 (deftest test-can-shape-fit?
   (testing "can-shape-fit?.."
     (is 
      (false? 
       (can-shape-fit? "I" 8 0 10)))))
 
+;;Testing the game engine with the provided example to check the correctness. 
 (deftest test-example-1
   (testing "Example1 : I0,I4,Q8"
     (is (= (-> (play-game "I0,I4,Q8" 10)
